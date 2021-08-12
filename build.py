859e1bb9547cfb968d38e45cc60d89e3c5ae2979
I_Love_Study <1450069615@qq.com>
2021-08-13 01:53:58 +0800

    Fix #6, Releases 0.0.15

--- build.py
+++ build.py
@@ -6,20 +6,13 @@ import shutil
 
 ext_modules = [
     Extension('graiax.silkcoder._silkv3',
-              sources=glob('graiax/silkcoder/_c_silkv3/src/*.c'),
-              include_dirs=["graiax/silkcoder/_c_silkv3/interface/"]
+              sources=glob('src/c_silkv3/src/*.c'),
+              include_dirs=["src/c_silkv3/interface/"]
               )]
 
 def build(setup_kwargs):
     """
     This function is mandatory in order to build the extensions.
     """
-    '''
-    distribution = Distribution(dict(name="graiax-silkcoder", ext_modules=ext_modules))
-    distribution.package_dir = r"graia/silkcoder"
-    ext = build_ext(distribution)
-    ext.ensure_finalized()
-    ext.run()
-    shutil.rmtree('graiax/silkcoder/_c_silkv3')
-    '''
-    setup_kwargs['ext_modules'] = ext_modules
+    setup_kwargs["ext_modules"] = ext_modules
+    setup_kwargs["zip_safe"]    = False
