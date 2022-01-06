2fd8dd9a354d58adfd04ed928519e69a5306097a
I Love Study <1450069615@qq.com>
2022-01-06 23:11:43 +0800

    Use pdm instead of poetry

--- build.py
+++ build.py
@@ -4,11 +4,13 @@ from glob import glob
 ext_modules = [
     Extension('graiax.silkcoder._silkv3',
               sources=glob('src/c_silkv3/src/*.c'),
-              include_dirs=["src/c_silkv3/interface/"]
-              )]
+              include_dirs=["src/c_silkv3/interface/"])
+]
+
 
 def build(setup_kwargs):
     """
     This function is mandatory in order to build the extensions.
     """
-    setup_kwargs["ext_modules"] = ext_modules
+
+    setup_kwargs.update(ext_modules=ext_modules)
